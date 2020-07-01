3a19ba6f6120e9b83bf2b118b1ca0a10319864da
ZonshenYu <55853237+ZonshenYu@users.noreply.github.com>
2020-07-01 03:00:51 -0400

    Add files via upload
    Improved bounds for random values

--- TPCompletion.py
+++ TPCompletion.py
@@ -83,15 +83,86 @@ def guessCompletableFilledIn(arr, trials):
 	for i in range(len(arr)):
 		for j in range(len(arr[0])):
 			if arr[i][j] == 0:
-				unspecList.append([i,j])
-				
-	#first, just a random search 
-	upperbound = np.amax(arr)*2 #this is worth experimenthing with
-	lowerbound = .001 #probably not good to go too low, it'll clash with the determinant bound for '0'
+				unspecList.append( (i,j) ) #the 3rd entry will be the lower bound, the 4th the upper bound of values (2^63-1)  0, 9223372036854775807
+	
+	#use 2 by 2 minors with 3 specified entries to derive better bounds per unspecified entry
+	lowerBounds = {} 
+	upperBounds = {}
+	
+	for i in range(len(arr)):
+		for j in range(len(arr[0])):
+			if arr[i][j] == 0:
+				continue 
+			for k in range(j+1, len(arr[0])):
+				if arr[i][k] == 0:
+					continue
+				for l in range(i+1, len(arr)):
+				#i is the upper row, l the lower row, j the left column, k the right column
+					if arr[l][j] > 0 and arr[l][k] == 0:
+						#looks like: 
+						#x x 
+						#x ? 
+						newlowerbound = arr[i][k]*arr[l][j]/arr[i][j]
+						if (l,k) in lowerBounds:
+							lowerBounds[(l,k)] = max(lowerBounds[(l,k)], newlowerbound)
+						else:
+							lowerBounds[(l,k)] = newlowerbound
+					
+					if arr[l][k] > 0 and arr[l][j] == 0:
+						#looks like: 
+						#x x 
+						#? x 
+						newupperbound = arr[i][j]*arr[l][k]/arr[i][k]
+						if (l,j) in upperBounds:
+							upperBounds[(l,j)] = min(upperBounds[(l,j)], newupperbound)
+						else:
+							upperBounds[(l,j)] = newupperbound 
+				for l in range(i):
+				#i is the lower row, l the upper row, j the left column, k the right column
+					if arr[l][j] == 0 and arr[l][k] > 0:
+						#looks like: 
+						#? x 
+						#x x 
+						newlowerbound = arr[i][j]*arr[l][k]/arr[i][k]
+						if (l,j) in lowerBounds:
+							lowerBounds[(l,j)] = max(lowerBounds[(l,j)], newlowerbound)
+						else:
+							lowerBounds[(l,j)] = newlowerbound
+					
+					if arr[l][k] == 0 and arr[l][j] > 0:
+						#looks like: 
+						#x ? 
+						#x x 
+						newupperbound = arr[i][k]*arr[l][j]/arr[i][j]
+						if (l,k) in upperBounds:
+							upperBounds[(l,k)] = min(upperBounds[(l,k)], newupperbound)
+						else:
+							upperBounds[(l,k)] = newupperbound 
+	
+	#tester for lower and upper bounds, note that these are 0 indexed
+#	print("LOWER BOUNDS:")	
+#	for x,y in lowerBounds.items():
+#		print(x,y)
+		
+#	print("UPPER BOUNDS:")	
+#	for x,y in upperBounds.items():
+#		print(x,y)	
+	#a random search 
+	GeneralUpperBound = np.amax(arr)*2 #this is worth experimenting with
+	GeneralLowerBound = .001 #probably not good to go too low, it'll clash with the determinant bound for '0'
 	foundComp = False 
 	for i in range(trials):
 		for entry in unspecList:
-			arr[entry[0]][entry[1]] = random.uniform(lowerbound, upperbound)
+			rowPos = entry[0]
+			colPos = entry[1]
+			lowerbound = GeneralLowerBound
+			upperBound = GeneralUpperBound 
+			if (rowPos, colPos) in lowerBounds:
+				lowerbound = lowerBounds[(rowPos, colPos)]
+			
+			if (rowPos, colPos) in upperBounds:
+				upperbound = upperBounds[(rowPos, colPos)]
+			arr[rowPos][colPos] = random.uniform(lowerbound, upperbound)
 		isTP = checkTP(arr, False)
 		if isTP:
 			print("Completion found")
@@ -102,10 +173,25 @@ def guessCompletableFilledIn(arr, trials):
 	if not foundComp:
 		print("A random search suggests this is not a completable partial matrix")
 	
+	#TODO: even better guessing, find immediate contradiction, although I don't think that will happen much, if at all
+	
+#takes in a pattern in the form of a (0,1)-matrix, 0 being unspecified, 1 being unspecified, and attempts to guess if the pattern is completable 	
+#not complete
+def guessCompletableNotFilledIn(arr):
+	arr = arr.astype('float64') #because we usually only input integers, without this line, python casts the random numbers below to integers too
+	unspecList = []
+	specList = []
+	for i in range(len(arr)):
+		for j in range(len(arr[0])):
+			if arr[i][j] == 0:
+				unspecList.append([i,j])
+			else:
+				specList.append([i,j])
+	
 def tester(): 
 	#np.set_printoptions(precision=1)
 	#print(np.array([1.124125]))
-	test = np.array([[1,1,1,0], [0,1,2, 1],[1, 2,0,3]])
+	test = np.array([[1, 2, 1], [0, 1, 1], [0, 1, 2]])
 	print(test)
 	#print(np.linalg.det(test))
 	#isTP = checkTP(test, True)
@@ -121,6 +207,6 @@ def tester():
 #		print("The matrix is TP")
 #	else:
 #		print("The matrix is not TP")
-	guessCompletableFilledIn(test, 10000)
+	guessCompletableFilledIn(test, 1000)
 
 tester()
